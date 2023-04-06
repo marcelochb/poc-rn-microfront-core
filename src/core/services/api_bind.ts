efcdd61d3001a71c6cba838a09405d74d9b3d60e
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-04-05 22:22:44 -0300

    feat: axios api

--- src/core/services/api_bind.ts
+++ src/core/services/api_bind.ts
@@ -2,6 +2,7 @@
 import { container } from "tsyringe"
 import { CoreConstants } from "../constants";
 import { FetchApiClient } from "./fetch_api_client";
+import { AxiosApiClient } from "./axios_api_client";
 
 export const apiDependences = () => {
   container.register(CoreConstants.IApiClient,{useValue: new FetchApiClient()});
